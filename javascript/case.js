6ca241f31277be7bc3ed29c6c62bcf91549a5597
Pranesh98 <praneshchy23@gmail.com>
2023-09-28 02:42:43 +0600

    plus and minus working

--- javascript/case.js
+++ javascript/case.js
@@ -1,17 +1,37 @@
 console.log("it's working.");
 
-document.getElementById('btn-case-plus').addEventListener('click', function(){
+function updateCaseNumber(isIncrease){
     const caseNumberField = document.getElementById('case-number-field');
     const caseNumberString = caseNumberField.value;
     console.log(caseNumberString);
     const previousCaseNumber = parseInt(caseNumberString);
     
-    const newCaseNumber= previousCaseNumber+1;
-    console.log(newCaseNumber)
+    let newCaseNumber;
+
+    if (isIncrease === true){
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else{
+        newCaseNumber = previousCaseNumber - 1;
+    }
     caseNumberField.value = newCaseNumber;
+    return newCaseNumber;
+};
+
+function updateCaseTotalNumber (newCaseNumber){
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+};
+
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+    const newCaseNumber = updateCaseNumber(true);
+    updateCaseTotalNumber (newCaseNumber);
 });
 
 document.getElementById('btn-case-minus').addEventListener('click', function(){
-    console.log("effort");
-})
+    const newCaseNumber = updateCaseNumber(false);
+
+    updateCaseTotalNumber (newCaseNumber);
+});
 
